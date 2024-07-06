94023deaa1f5a05484e0eb817ff85011ce3b155e
Delano Muongchan <144065012+delanomuongchan@users.noreply.github.com>
2024-07-05 17:07:37 -0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "todo-react",
+  base: "/todo-react/",
   plugins: [react()],
 })
